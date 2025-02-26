a97263f73ef2305fde37d907a8ed0defaf033d32
Opyu <wayne.jiawei.shi@gmail.com>
2025-02-26 11:38:48 +0000

    Initial commit

---
+++ lua/options.lua
@@ -0,0 +1,9 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+vim.cmd('set expandtab')
+vim.cmd('set tabstop=4')
+vim.cmd('set shiftwidth=4 smarttab')
